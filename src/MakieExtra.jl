db19bc64479a6cd14d17553399100bce75cb67a3
Alexander Plavin <alexander@plav.in>
2024-10-23 00:00:00 +0000

    add scalebar

--- src/MakieExtra.jl
+++ src/MakieExtra.jl
@@ -5,6 +5,9 @@ using Accessors
 using InverseFunctions
 using PyFormattedStrings
 using Makie.IntervalSets
+using Makie.IntervalSets: width
+using Makie.Unitful
+using DataPipes
 
 @reexport using Makie
 export Makie
@@ -13,7 +16,16 @@ export SymLog, AsinhScale, BaseMulTicks, EngTicks
 
 include("scales.jl")
 include("ticks.jl")
+include("scalebar.jl")
 include("helpers.jl")
 include("axisfunction.jl")
+
+
+# XXX: should upstream
+function Accessors.set(attrs::Attributes, il::IndexLens, val)
+	res = deepcopy(attrs)
+	res[only(il.indices)] = val
+	return res
+end
 
 end
