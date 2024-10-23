89be71d3e1907bf9a21db671581e917486100ef7
Alexander Plavin <alexander@plav.in>
2024-10-23 00:00:00 +0000

    fix zoomlines to properly account transformations, reverses, etc

--- src/zoom_lines.jl
+++ src/zoom_lines.jl
@@ -1,31 +1,33 @@
 function zoom_lines!(ax1, ax2; strokewidth=1.5, strokecolor=:black, color=(:black, 0))
     pscene = parent(parent(Makie.parent_scene(ax1)))
     @assert parent(parent(Makie.parent_scene(ax2))) === pscene
-    obs = @lift let
-        lims = [$(ax1.finallimits), $(ax2.finallimits)]
-        vps = [$(ax1.scene.viewport), $(ax2.scene.viewport)]
-        axs = lims[1] ⊆ lims[2] ? (
-            (vp=vps[1], lim=lims[1]),
-            (vp=vps[2], lim=lims[2]),
-        ) : lims[2] ⊆ lims[1] ? (
-            (vp=vps[2], lim=lims[2]),
-            (vp=vps[1], lim=lims[1]),
-        ) : nothing
-        fs = isnothing(axs) ? nothing : if right(axs[1].vp) < left(axs[2].vp)
-            (topright, bottomright, topleft, bottomleft)
-        elseif left(axs[1].vp) > right(axs[2].vp)
-            (topleft, bottomleft, topright, bottomright)
-        elseif bottom(axs[1].vp) < top(axs[2].vp)
-            (topleft, topright, bottomleft, bottomright)
-        elseif bottom(axs[1].vp) > top(axs[2].vp)
-            (bottomleft, bottomright, topleft, topright)
-        end
+    obs = lift(ax1.finallimits, ax2.finallimits, ax1.scene.viewport, ax2.scene.viewport, ax1.scene.camera.projectionview, ax2.scene.camera.projectionview, Makie.transform_func(ax1), Makie.transform_func(ax2)) do _...
+        lims = [ax1.finallimits[], ax2.finallimits[]]
+		axs = lims[1] ⊆ lims[2] ? (ax1, ax2) :
+			  lims[2] ⊆ lims[1] ? (ax2, ax1) :
+			  nothing
+        if isnothing(axs)
+			nothing
+		else
+			vps = to_value.(viewport.(axs))
+			fs = if right(vps[1]) < left(vps[2])
+	            (topright, bottomright, topline, bottomline)
+	        elseif left(vps[1]) > right(vps[2])
+	            (topleft, bottomleft, topline, bottomline)
+	        elseif bottom(vps[1]) < top(vps[2])
+	            (topleft, topright, leftline, rightline)
+	        elseif bottom(vps[1]) > top(vps[2])
+	            (bottomleft, bottomright, leftline, rightline)
+	        end
+			line1 = fullproject.(Ref(axs[2]), fs[3](axs[1].finallimits[]))
+			line2 = fullproject.(Ref(axs[2]), fs[4](axs[1].finallimits[]))
+		end
         (
-            rect1=$(ax2.finallimits),
-            rect2=$(ax1.finallimits),
-            slines=isnothing(axs) || isnothing(fs) ? Point2{Float32}[] : [
-                fs[1](axs[1].vp), shift_range(fs[3](axs[1].lim), axs[2].lim => axs[2].vp),
-                fs[2](axs[1].vp), shift_range(fs[4](axs[1].lim), axs[2].lim => axs[2].vp),
+            rect1=ax2.finallimits[],
+            rect2=ax1.finallimits[],
+            slines=isnothing(axs) ? Point2{Float32}[] : Point2{Float32}[
+				fs[1](vps[1]), argmin(p -> norm(p - fs[1](vps[1])), line1),
+                fs[2](vps[1]), argmin(p -> norm(p - fs[2](vps[1])), line2),
             ],
         )
     end
