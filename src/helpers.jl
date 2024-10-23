e603f370054c607924213ea9414231799361c246
Alexander Plavin <alexander@plav.in>
2024-10-23 00:00:00 +0000

    add macro define_plotfunc_conv

--- src/helpers.jl
+++ src/helpers.jl
@@ -34,9 +34,63 @@ macro define_plotfunc(plotfuncs, Ts)
     Expr(:block, exprs...)
 end
 
+
+macro define_plotfunc_conv(plotfuncs, Ts)
+    plotfuncs = plotfuncs isa Symbol ? [plotfuncs] :
+                Base.isexpr(plotfuncs, :tuple) ? plotfuncs.args :
+                error()
+
+    Ts = Base.isexpr(Ts, :tuple) ? Ts.args : [Ts]
+    Ts = esc.(Ts)
+
+    exprs = map(plotfuncs) do plotf
+        plotf_excl = Symbol(plotf, :!)
+		plottype = :($Plot{$plotf})
+        argnames = map(i -> Symbol(:x, i) |> esc, 1:length(Ts))
+        args_any = map((n, T) -> :($n::Union{$T,Observable{<:$T}}), argnames, Ts)
+		@assert length(argnames) == 1
+		argname = only(argnames)
+        quote
+            function Makie.$plotf(pos::Union{GridPosition, GridSubposition}, $(args_any...); kwargs...)
+				used_attrs = used_attributes($plottype, Makie.to_value($argname))
+				$plotf(
+					pos,
+					_lift(
+						x -> _convert_arguments_singlestep($plottype, x; _select_kwargs(kwargs, used_attrs)...) |> only,
+						$argname);
+					_unselect_kwargs(kwargs, used_attrs)...
+				)
+			end
+
+            function Makie.$plotf($(args_any...); kwargs...)
+				used_attrs = used_attributes($plottype, Makie.to_value($argname))
+				$plotf(
+					_lift(
+						x -> _convert_arguments_singlestep($plottype, x; _select_kwargs(kwargs, used_attrs)...) |> only,
+						$argname);
+					_unselect_kwargs(kwargs, used_attrs)...
+				)
+			end
+
+			Makie.convert_arguments(ct::Type{<:$plottype}, $(args_any...); kwargs...) =
+				convert_arguments(ct, _convert_arguments_singlestep(ct, $(argnames...); kwargs...)...)
+        end
+    end
+    Expr(:block, exprs...)
+end
+
+
+_lift(f, x::Observable) = lift(f, x)
+_lift(f, x) = f(x)
+
 _ensure_observable(x) = Observable(x)
 _ensure_observable(x::Observable) = x
 
-function default_axis_attributes end
+_select_kwargs(kwargs, ks) = kwargs[convert(Vector{Symbol}, intersect(keys(kwargs), ks))]
+_unselect_kwargs(kwargs, ks) = kwargs[convert(Vector{Symbol}, setdiff(keys(kwargs), ks))]
 
+
+function default_axis_attributes end
 default_axis_attributes(T, args::Vararg{Observable}; kwargs...) = default_axis_attributes(T, getindex.(args)...; kwargs...)
+
+function _convert_arguments_singlestep end
