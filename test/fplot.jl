9e56824fb76a1b5c83d154b8d9e6ed3aca78ab85
Alexander Plavin <alexander@plav.in>
2024-10-23 00:00:00 +0000

    split fplot tests into separate file

---
+++ test/fplot.jl
@@ -0,0 +1,80 @@
+@testitem "fplot structure" begin
+    using Accessors
+
+    fp = FPlot(1:10, (@o _+1), (@o _^2), color=sqrt)
+    @test values(fp) === 1:10
+    @test fp.color === sqrt
+    @test fp[1] === @o _+1
+    @test fp[2] === @o _^2
+    @test fp[:color] === sqrt
+    @test (@set values(fp) = [1,2,3]) == FPlot([1,2,3], (@o _+1), (@o _^2), color=sqrt)
+    @test (@set fp.color = sin) === FPlot(1:10, (@o _+1), (@o _^2), color=sin)
+    @test (@set fp[:color] = sin) === FPlot(1:10, (@o _+1), (@o _^2), color=sin)
+    @test (@insert fp.markersize = sin) === FPlot(1:10, (@o _+1), (@o _^2), color=sqrt, markersize=sin)
+    @test (@set fp[1] = @o _+2) === FPlot(1:10, (@o _+2), (@o _^2), color=sqrt)
+    @test (@delete values(fp)) === FPlot(nothing, (@o _+1), (@o _^2), color=sqrt)
+    fp_nov = FPlot(nothing, (@o _+1), (@o _^2), color=sqrt)
+    @test (@insert values(fp_nov) = 1:10) === FPlot(1:10, (@o _+1), (@o _^2), color=sqrt)
+end
+
+@testitem "fplot basic" begin
+    using Accessors
+
+    fig, ax, plt = lines(FPlot(1:10, (@o _+1), (@o _^2), color=sqrt), doaxis=true)
+    @test content(fig[1,1]).xlabel[] == "+(_, 1)"
+    fig, ax, plt = lines(FPlot(1:10, (@o _+1), (@o _^2), color=sqrt), linewidth=10)
+    @test ax.xlabel[] == ""
+    @test plt.linewidth[] == 10
+    plt = lines!(FPlot(1:10, (@o _+1), (@o _^2), color=sqrt))
+    plt = lines!(FPlot(1:10, (@o _+1), (@o _^2), color=Ref(:black)), doaxis=true)
+    plt = lines!(FPlot(1:10, x->x+1, (@o _^2), color=sqrt), linewidth=15)
+    @test plt.linewidth[] == 15
+    plt = lines!(1:10, FPlot(x->x+1, (@o _^2), color=sqrt), linewidth=15)
+    @test ax.xlabel[] == ""
+    @test plt.linewidth[] == 15
+    plt = lines!(1:10, FPlot(x->x+1, (@o _^2), color=sqrt, markersize=identity), linewidth=15)
+end
+
+@testitem "flipped xy" begin
+    using Accessors
+
+    barplot(FPlot(1:10, (@o _), (@o _^2)), doaxis=true)
+    @test current_axis().xlabel[] == "identity"
+    @test current_axis().ylabel[] == "^(_, 2)"
+    barplot(FPlot(1:10, (@o _), (@o _^2)), direction=:x, doaxis=true)
+    @test current_axis().xlabel[] == "^(_, 2)"
+    @test current_axis().ylabel[] == "identity"
+end
+
+    # fig, ax, plt = lines(FPlot(1:10, (@o _+1), (@o _^2), color=sqrt, axis=(xlabel="Abcdef", yscale=log10)), linewidth=10, doaxis=true)
+    # ax = content(fig[1,1])
+    # @test ax.xlabel[] == "Abcdef"
+    # @test ax.ylabel[] == "^(_, 2)"
+    # @test ax.yscale[] == log10
+    # fig, ax, plt = lines(FPlot(1:10, (@o _+1), (@o _^2), color=sqrt, axis=(xlabel="Abcdef", yscale=log10)), linewidth=10, doaxis=true, axis=(ylabel="Def",))
+    # ax = content(fig[1,1])
+    # @test ax.xlabel[] == "Abcdef"
+    # @test ax.ylabel[] == "Def"
+    # @test ax.yscale[] == log10
+    # fig, ax, plt = lines(FPlot(1:10, (@o _+1), (@o _^2), color=sqrt, axis=(xlabel="Abcdef", yscale=log10)), linewidth=10, doaxis=false, axis=(ylabel="Def",))
+    # ax = content(fig[1,1])
+    # @test ax.xlabel[] == ""
+    # @test ax.ylabel[] == "Def"
+    # @test ax.yscale[] == identity
+    # fig, ax, plt = lines(FPlot(1:10, (@o _+1), (@o _^2), color=sqrt, axis=(xlabel="Abcdef", yscale=log10)), linewidth=10, doaxis=false)
+    # @test ax.xlabel[] == ""
+    # @test ax.ylabel[] == ""
+    # @test ax.yscale[] == identity
+
+@testitem "conversion to FPlot" begin
+    using Accessors
+
+    struct MyObj end
+    Makie.used_attributes(T::Type{<:Plot}, ::MyObj) = Tuple(Makie.attribute_names(T))
+    Makie.convert_arguments(ct::Type{<:AbstractPlot}, ::MyObj; kwargs...) = Makie.convert_arguments(ct, FPlot(1:10, (@o _+1), (@o _^2), color=sqrt); doaxis=true, kwargs...)
+
+    lines(MyObj(); linewidth=15)
+    lines(MyObj())
+    lines!(MyObj())
+    lines!(MyObj(), linewidth=10)
+end
