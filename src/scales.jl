6c0475b6365d203975e7502a40c087d501407437
Alexander Plavin <alexander@plav.in>
2024-10-26 00:00:00 +0000

    nicer show()

--- src/scales.jl
+++ src/scales.jl
@@ -53,3 +53,26 @@ const SymLogLike = Union{SymLog,AsinhScale}
 
 Makie.defaultlimits(s::SymLogLike) = clamp.((-3, 3), s.vmin, s.vmax)
 Makie.defined_interval(::SymLogLike) = -Inf..Inf
+
+
+Base.show(io::IO, s::SymLog) = print(io, "SymLog($(s.linthresh)$(remove_defaults_str(s)))")
+Base.show(io::IO, s::AsinhScale) = print(io, "AsinhScale($(s.linthresh)$(remove_defaults_str(s)))")
+
+remove_defaults_str(s) = @p let
+    remove_defaults(s)
+    string()
+    strip(__, ['(', ')', ','])
+    __ == "NamedTuple" ? "" : "; $__"
+end
+remove_defaults(s::SymLog) = @p let
+    Accessors.getproperties(s)[(:base, :linscale, :vmin, :vmax)]
+    __.base == 10 ? (@delete __.base) : __
+    __.linscale == 1 ? (@delete __.linscale) : __
+    __.vmin == -Inf ? (@delete __.vmin) : __
+    __.vmax == Inf ? (@delete __.vmax) : __
+end
+remove_defaults(s::AsinhScale) = @p let
+    Accessors.getproperties(s)[(:vmin, :vmax)]
+    __.vmin == -Inf ? (@delete __.vmin) : __
+    __.vmax == Inf ? (@delete __.vmax) : __
+end
