d1b53dbb23c78fe19ea05b71c1781426d2d6a956
Alexander Plavin <alexander@plav.in>
2024-12-27 00:00:00 +0000

    fix axisfunction for more plot types

--- src/axisfunction.jl
+++ src/axisfunction.jl
@@ -1,9 +1,20 @@
-@define_plotfunc (scatter, lines, scatterlines, band, stairs, stem, barplot) Function 
+@define_plotfunc (
+	scatter, lines, scatterlines,
+	stairs, stem,
+	errorbars, rangebars, band,
+	barplot,
+	linesglow, bandstroke) Function
 
-for plotf in (:scatter, :lines, :scatterlines, :band, :errorbars, :rangebars, :stairs, :stem, :barplot)
+for plotf in (
+		:scatter, :lines, :scatterlines,
+		:stairs, :stem,
+		:errorbars, :rangebars, :band,
+		:barplot,
+		:linesglow, :bandstroke)
 	plotf_excl = Symbol(plotf, :!)
 
-	@eval function Makie.$plotf_excl(ax::Axis, f::Union{Function,Observable{<:Function}}; kwargs...)
+	fullname_excl = isdefined(Makie, plotf) ? :(Makie.$plotf_excl) : :($(@__MODULE__).$plotf_excl)
+	@eval function $fullname_excl(ax::Axis, f::Union{Function,Observable{<:Function}}; kwargs...)
 		interval = lift(xint, ax.finallimits)
 		$plotf_excl(ax, interval, f; merge((; xautolimits=false), kwargs)...)
 	end
