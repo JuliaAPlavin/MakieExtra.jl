4168753a1b8631c46374f34f8a7a78556ed82175
Alexander Plavin <alexander@plav.in>
2024-12-27 00:00:00 +0000

    support extra axis attributes in FPlot

--- src/fplot.jl
+++ src/fplot.jl
@@ -2,10 +2,11 @@ struct FPlot
 	data
 	argfuncs::Tuple
 	kwargfuncs::NamedTuple
+	axis::NamedTuple
 end
 @batteries FPlot
 
-FPlot(data, argfuncs...; kwargsfuncs...) = FPlot(data, argfuncs, NamedTuple(kwargsfuncs))
+FPlot(data, argfuncs...; axis::NamedTuple=(;), kwargsfuncs...) = FPlot(data, argfuncs, NamedTuple(kwargsfuncs), axis)
 
 @accessor Base.values(X::FPlot) = X.data
 Accessors.delete(X::FPlot, ::typeof(values)) = @set X.data = nothing
@@ -20,12 +21,13 @@ Base.getproperty(X::FPlot, i::Symbol) = hasfield(typeof(X), i) ? getfield(X, i) 
 Base.propertynames(X::FPlot) = (fieldnames(typeof(X))..., keys(X.kwargfuncs)...)
 function Accessors.setproperties(X::FPlot, patch::NamedTuple)
 	fnames = Tuple(fieldnames(typeof(X)) ∩ keys(patch))
-	props = (;
+	fields = (;
 		getproperties(X)...,
 		patch[fnames]...,
 		kwargfuncs = merge(X.kwargfuncs, get(patch, :kwargfuncs, (;)), @delete patch[fnames]),
 	)
-	FPlot(props.data, props.argfuncs, props.kwargfuncs)
+	@assert keys(fields) == fieldnames(typeof(X))
+	FPlot(fields...)
 end
 Accessors.insert(X::FPlot, p::PropertyLens, v) = @insert X.kwargfuncs |> p = v
 
@@ -47,12 +49,16 @@ function Makie.convert_arguments(ct::Type{<:AbstractPlot}, X::FPlot; doaxis=fals
 	if doaxis
 		S = Makie.SpecApi
 		# can set axis attributes (eg xylabels), but cannot be plotted on existing axes
-		S.GridLayout([S.Axis(plots=[pspec], xlabel=_xlabel(X), ylabel=_ylabel(X))])
+		S.GridLayout([S.Axis(plots=[pspec]; axis_attributes(X)...)])
 	else
 		# can be plotted either from scratch or on existing axes, but cannot set axis attributes
 		pspec
 	end
 end
+
+# Makie.plot!(ax::Axis, plot::Plot{plot, Tuple{Makie.GridLayoutSpec}}) = plotlist!(ax, plot.converted[][].content[].second.plots[])
+
+axis_attributes(X::FPlot) = (; xlabel=_xlabel(X), ylabel=_ylabel(X), X.axis...)
 
 @inline getval(f, data) = map(f, data)
 @inline getval(f::Ref, data) = f[]
