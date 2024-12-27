cdc4979fd597b764430aaadbdc24e8d971b6f778
Alexander Plavin <alexander@plav.in>
2024-12-27 00:00:00 +0000

    even further fix zoomlines

--- src/zoom_lines.jl
+++ src/zoom_lines.jl
@@ -3,24 +3,24 @@ function zoom_lines!(ax1, ax2; strokewidth=1.5, strokecolor=:black, color=(:blac
     @assert parent(parent(Makie.parent_scene(ax2))) === pscene
     obs = lift(ax1.finallimits, ax2.finallimits, ax1.scene.viewport, ax2.scene.viewport, ax1.scene.camera.projectionview, ax2.scene.camera.projectionview, Makie.transform_func(ax1), Makie.transform_func(ax2)) do _...
         lims = [ax1.finallimits[], ax2.finallimits[]]
-		axs = lims[1] ⊆ lims[2] ? (ax1, ax2) :
-			  lims[2] ⊆ lims[1] ? (ax2, ax1) :
-			  nothing
+        axs = lims[1] ⊆ lims[2] ? (ax1, ax2) :
+              lims[2] ⊆ lims[1] ? (ax2, ax1) :
+              nothing
         slines = if isnothing(axs)
-			nothing
-		else
-			r1 = viewport(axs[1])[]
-			r2 = fullproject(axs[2], axs[1].finallimits[])
+            nothing
+        else
+            r1 = fullproject(axs[1], axs[1].finallimits[])
+            r2 = fullproject(axs[2], axs[1].finallimits[])
             cornsets = [
-				((corner(r1, (1,1)), corner(r2, (-1,1))), (corner(r1, (1,-1)), corner(r2, (-1,-1)))),
-				((corner(r1, (1,-1)), corner(r2, (1,1))), (corner(r1, (-1,-1)), corner(r2, (-1,1)))),
-				((corner(r1, (-1,-1)), corner(r2, (1,-1))), (corner(r1, (-1,1)), corner(r2, (1,1)))),
-				((corner(r1, (-1,1)), corner(r2, (-1,-1))), (corner(r1, (1,1)), corner(r2, (1,-1)))),
-			]
-			argmin(cornsets) do ((a1, a2), (b1, b2))
-				min(norm(a1-a2), norm(b1-b2))
-			end
-		end
+                ((corner(r1, (1,1)), corner(r2, (-1,1))), (corner(r1, (1,-1)), corner(r2, (-1,-1)))),
+                ((corner(r1, (1,-1)), corner(r2, (1,1))), (corner(r1, (-1,-1)), corner(r2, (-1,1)))),
+                ((corner(r1, (-1,-1)), corner(r2, (1,-1))), (corner(r1, (-1,1)), corner(r2, (1,1)))),
+                ((corner(r1, (-1,1)), corner(r2, (-1,-1))), (corner(r1, (1,1)), corner(r2, (1,-1)))),
+            ]
+            argmin(cornsets) do ((a1, a2), (b1, b2))
+                min(norm(a1-a2), norm(b1-b2))
+            end
+        end
         (
             rect1=ax2.finallimits[],
             rect2=ax1.finallimits[],
