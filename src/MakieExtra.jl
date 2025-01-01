22262adff1a6ce1272efad47bfb24f2a0d6207da
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    remove method override, enable precompilation

--- src/MakieExtra.jl
+++ src/MakieExtra.jl
@@ -13,19 +13,4 @@ include("scales.jl")
 include("ticks.jl")
 include("helpers.jl")
 
-
-# almost https://github.com/MakieOrg/Makie.jl/pull/3697
-__precompile__(false)
-function Base.show(io::IO, ::MIME"text/html", vs::VideoStream)
-    mktempdir() do dir
-        path = save(joinpath(dir, "video.mp4"), vs)
-        print(
-            io,
-            """<video autoplay controls loop><source src="data:video/x-m4v;base64,""",
-            Makie.base64encode(open(read, path)),
-            """" type="video/mp4"></video>"""
-        )
-    end
-end
-
 end
