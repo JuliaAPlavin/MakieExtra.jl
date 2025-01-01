26ee0c4c1acfcd305604c86e153690ff245ee2ab
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    add radiobuttons()

--- test/runtests.jl
+++ test/runtests.jl
@@ -553,6 +553,15 @@ end
     @test string("abc " * rich("x", color=:red)) == string(rich("abc ", rich("x", color=:red)))
 end
 
+@testitem "radiobuttons" begin
+    # smoke test only
+    fig = Figure()
+    rbs = MakieExtra.radiobuttons(Checkbox(fig[1,1]), Checkbox(fig[1,1], checked=true), Checkbox(fig[1,1]))
+    @test rbs.selix[] == 2
+    rbs = MakieExtra.radiobuttons(Checkbox(fig[1,1], checked=true) => Button(fig[1,2]), Checkbox(fig[1,1]) => Button(fig[1,2]), Checkbox(fig[1,1]) => Button(fig[1,2]))
+    @test rbs.selix[] == 1
+end
+
 @testitem "geoaxis_radians" begin
     # smoke test only
     MakieExtra.GeoAxis_radians!()
