f41e2ece1af0a7c1ff0063b32060fd1a521d2d37
Alexander Plavin <alexander@plav.in>
2025-01-01 00:00:00 +0000

    add contourf_fast

--- src/MakieExtra.jl
+++ src/MakieExtra.jl
@@ -22,6 +22,7 @@ include("scalebar.jl")
 include("zoom_lines.jl")
 include("helpers.jl")
 include("axisfunction.jl")
+include("contourf.jl")
 
 
 # XXX: should upstream these!
@@ -31,6 +32,19 @@ function Accessors.set(attrs::Attributes, il::IndexLens, val)
 	res[only(il.indices)] = val
 	return res
 end
+
+function Accessors.insert(attrs::Attributes, il::IndexLens, val)
+	res = deepcopy(attrs)
+	res[only(il.indices)] = val
+	return res
+end
+
+function Accessors.delete(attrs::Attributes, il::IndexLens)
+	res = deepcopy(attrs)
+	delete!(res, only(il.indices))
+	return res
+end
+
 
 Base.:(⊆)(a::Rect2, b::Rect2) = xint(a) ⊆ xint(b) && yint(a) ⊆ yint(b)
 
