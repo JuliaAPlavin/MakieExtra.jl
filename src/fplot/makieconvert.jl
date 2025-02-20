756f89ac049a9f619d29d9d49929b4cd03258c2a
Alexander Plavin <alexander@plav.in>
2025-02-20 13:39:57 +0000

    minor upds

--- src/fplot/makieconvert.jl
+++ src/fplot/makieconvert.jl
@@ -19,7 +19,7 @@ end
 @inline getval(data, k, f) =
     isempty(methods(f)) ? f :
     k == :inspector_label ? (self, i, p) -> f(data[i]) :
-    k == :inspector_hover ? (self, p, i) -> f(data[i]) :
+    k == :inspector_hover ? (self, p, i, _...) -> f(data[i]) :
     map(f, data)
 @inline getval(data, _, f::Ref) = f[]
 
