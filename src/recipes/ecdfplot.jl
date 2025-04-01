93321bd3b61c52fbd8f1c69a416184845b43d862
Alexander Plavin <alexander@plav.in>
2025-04-01 00:00:00 +0000

    add ecdfplotfull

---
+++ src/recipes/ecdfplot.jl
@@ -0,0 +1,45 @@
+@recipe ECDFPlotFull (xint::Union{Nothing,Interval}, values::AbstractVector{<:Number}) begin
+    rev = false
+    documented_attributes(Lines)...
+    @modify($(documented_attributes(Scatter)).d) do d
+        filter_keys(∉(keys(documented_attributes(Lines).d)), d)
+    end...
+end
+
+function Makie.plot!(p::ECDFPlotFull)
+    vs = @lift sort($(p.values); rev=$(p.rev))
+    n = @lift length($vs)
+    
+    lrval = @lift if !isnothing($(p.xint))
+        eps = extrema($(p.xint))
+        $(p.rev) ? reverse(eps) : eps
+    else
+        (first($vs), last($vs))
+    end
+
+    # xautolimits=false below don't seem to work
+    # interval = @something p.xint[] let
+    # 	scene = Makie.get_scene(p)
+    #     lift(scene.camera.projectionview, p.model, Makie.transform_func(p), scene.viewport) do _...
+    # 		left, _ = Makie.project(scene, :relative, :data, Point(0,0))
+    # 		right, _ = Makie.project(scene, :relative, :data, Point(1,0))
+    # 		left..right
+    #     end
+    # end
+    
+
+    scatter!(
+        p, Makie.shared_attributes(p, Scatter),
+        vs, @lift (1:$n) ./ $n)
+
+    lines!(
+        p, Makie.shared_attributes(p, Lines),
+        (@lift [first($lrval); repeat($vs, inner=2); last($lrval)]),
+        (@lift repeat((0:$n) ./ $n, inner=2));
+        # xautolimits=false  # XXX: doesn't work at all
+    )
+
+    return p
+end
+
+Makie.convert_arguments(ct::Type{ECDFPlotFull}, values::AbstractVector{<:Number}) = convert_arguments(ct, nothing, values)
