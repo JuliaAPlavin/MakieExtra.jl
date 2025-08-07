40824e19c79ad4267c4d813b58d5931ec2447a4f
Alexander Plavin <alexander@plav.in>
2025-08-07 20:38:03 +0000

    axplot_many: optional x/y axis linking; support non-numeric colors; WIP on observable support

--- src/fplot/axplot_many.jl
+++ src/fplot/axplot_many.jl
@@ -15,29 +15,31 @@ function axplot_many(plotf, fplt::FPlot; kwargs...)
     fig
 end
 
-function axplot_many(pos::Union{GridPosition,GridSubposition}, plotf, fplt::FPlot; legend=(;))
+function axplot_many(pos::Union{GridPosition,GridSubposition}, plotf, fplt::Union{Observable{<:FPlot},FPlot}; legend=(;), linkxaxes=true, linkyaxes=true)
     gl = GridLayout(pos)
-    if hasproperty(fplt, :color)
-        colorrange = @p fplt.data extrema(fplt.color)
+	fplt₀ = to_value(fplt)
+	color_isnumeric = hasproperty(fplt₀, :color) && fplt₀.color(first(fplt₀.data)) isa Number
+    common_kws = (;)
+    if hasproperty(fplt₀, :color) && color_isnumeric
+        colorrange = @p fplt₀.data extrema(fplt₀.color)
         common_kws = (;colorrange)
     end
-    common_kws = (;)
-    if any(f -> f isa ToAes && f.aes == :row, fplt.argfuncs)
-        ix = findonly(f -> f isa ToAes && f.aes == :row, fplt.argfuncs)
-        af = fplt.argfuncs[ix]
+    if any(f -> f isa ToAes && f.aes == :row, fplt₀.argfuncs)
+        ix = findonly(f -> f isa ToAes && f.aes == :row, fplt₀.argfuncs)
+        af = fplt₀.argfuncs[ix]
         for (irow, f) in enumerate(af.funcs)
             fplt_cur = @set fplt[ix] = f
             @p fplt.data group_vg(fplt.col) enumerate map() do (igr, gr)
                 _to_axplot(plotf)(gl[irow, igr], (@set fplt_cur.data = gr); common_kws...,
                                     axis=(;title=irow == 1 ? "$(shortlabel(fplt.col)) = $(key(gr))" : ""))
             end
-            ix == 1 && linkxaxes!(contents(gl[irow, :])...)
-            ix == 2 && linkyaxes!(contents(gl[irow, :])...)
+            linkxaxes && ix == 1 && linkxaxes!(contents(gl[irow, :])...)
+            linkyaxes && ix == 2 && linkyaxes!(contents(gl[irow, :])...)
         end
-        ix != 1 && linkxaxes!(contents(gl[:, :])...)
-        ix != 2 && linkyaxes!(contents(gl[:, :])...)
+        linkxaxes && ix != 1 && linkxaxes!(contents(gl[:, :])...)
+        linkyaxes && ix != 2 && linkyaxes!(contents(gl[:, :])...)
     else
-        if haskey(fplt, :col) && haskey(fplt, :row)
+        if haskey(fplt₀, :col) && haskey(fplt₀, :row)
             colixs = @p fplt.data map(fplt.col) unique Dict(__ .=> 1:length(__))
             rowixs = @p fplt.data map(fplt.row) unique Dict(__ .=> 1:length(__))
             @p fplt.data group_vg(x -> (;col=fplt.col(x), row=fplt.row(x))) map() do gr
@@ -48,25 +50,25 @@ function axplot_many(pos::Union{GridPosition,GridSubposition}, plotf, fplt::FPlo
                     gl[ir, ic],
                     (@set fplt.data = gr); common_kws..., axis=(;title=(ic == 1 ? rowlabel : rich(rowlabel; color=:gray70)) * "\n" * (ir == 1 ? collabel : rich(collabel; color=:gray70))))
             end
-            linkxaxes!(contents(gl[:, :])...)
-            linkyaxes!(contents(gl[:, :])...)
+            linkxaxes && linkxaxes!(contents(gl[:, :])...)
+            linkyaxes && linkyaxes!(contents(gl[:, :])...)
         elseif haskey(fplt, :col)
             @p fplt.data group_vg(fplt.col) enumerate map() do (igr, gr)
                 _to_axplot(plotf)(gl[1, igr], (@set fplt.data = gr); common_kws..., axis=(;title="$(shortlabel(fplt.col)) = $(key(gr))"))
             end
-            linkxaxes!(contents(gl[:, :])...)
-            linkyaxes!(contents(gl[:, :])...)
+            linkxaxes && linkxaxes!(contents(gl[:, :])...)
+            linkyaxes && linkyaxes!(contents(gl[:, :])...)
         elseif haskey(fplt, :row)
             @p fplt.data group_vg(fplt.row) enumerate map() do (igr, gr)
                 _to_axplot(plotf)(gl[igr, 1], (@set fplt.data = gr); common_kws..., axis=(;title="$(shortlabel(fplt.row)) = $(key(gr))"))
             end
-            linkxaxes!(contents(gl[:, :])...)
-            linkyaxes!(contents(gl[:, :])...)
+            linkxaxes && linkxaxes!(contents(gl[:, :])...)
+            linkyaxes && linkyaxes!(contents(gl[:, :])...)
         end
     end
-    autohide_axlabels!(gl[:, :])
+    autohide_axlabels!(gl[:, :]; hidex=linkxaxes, hidey=linkyaxes)
 
-    if hasproperty(fplt, :color) && get(legend, :color, true)
+    if hasproperty(fplt, :color) && get(legend, :color, true) && color_isnumeric
         Label(gl[:,end+1][1,1], shortlabel(fplt.color))
         kws = (;)
         if hasproperty(fplt, :colormap)
