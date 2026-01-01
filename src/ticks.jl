ab9b4f8b1cec5f89ba40241bbb0853ea6eb5281e
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    improve BaseMulTicks default behavior

--- src/ticks.jl
+++ src/ticks.jl
@@ -1,6 +1,7 @@
 @kwdef struct BaseMulTicks
-    subs = [1, 2, 5]
+    subs = nothing
     base = 10.
+    k_min::Int = 7
 end
 BaseMulTicks(subs; kwargs...) = BaseMulTicks(; subs, kwargs...)
 
@@ -8,28 +9,50 @@ function Makie.get_tickvalues(t::BaseMulTicks, vmin, vmax)
     vmin < vmax || return []
     vmin < 0 && vmax ≤ 0 && return .-Makie.get_tickvalues(t, -vmax, -vmin)
     @assert vmin ≥ 0 && vmax ≥ 0
-    @p [
-        mul * t.base^pow
-        for pow in floor(Int, log(t.base, vmin) - 0.1):ceil(Int, log(t.base, vmax) + 0.1)
-        for mul in t.subs
-    ] filter!(∈(vmin..vmax)) map(round(_, sigdigits=4)) map(isinteger(_) ? Int(_) : _)
+    if !isnothing(t.subs)
+        @p [
+            mul * t.base^pow
+            for pow in floor(Int, log(t.base, vmin) - 0.1):ceil(Int, log(t.base, vmax) + 0.1)
+            for mul in t.subs
+        ] filter!(∈(vmin..vmax)) map(round(_, sigdigits=4)) map(isinteger(_) ? Int(_) : _)
+    else
+        for subs in [[1], [1,3], [1,2,5], [1,2,3,5], 1:9]
+            ticks = Makie.get_tickvalues((@set t.subs = subs), vmin, vmax)
+            length(ticks) ≥ t.k_min && return ticks
+        end
+        return ticks
+    end
 end
 
 function Makie.get_tickvalues(t::BaseMulTicks, scale::SymLogLike, vmin, vmax; go_to_previous_base_power=false)
-    n_linticks = max(1.1, ceil(length(t.subs) * (@oget scale.linscale 0.0)))
-    mintick = @p let
-        min(scale.linthresh / n_linticks, max(abs(vmax), abs(vmin)))
-        go_to_previous_base_power ? @modify(l -> floor(l - 0.01) - 0.01, log(t.base, $__)) : __
-        max(vmin, __)
+    if !isnothing(t.subs)
+        n_linticks = max(1.1, ceil(length(t.subs) * (@oget scale.linscale 0.0)))
+        mintick = @p let
+            min(scale.linthresh / n_linticks, max(abs(vmax), abs(vmin)))
+            go_to_previous_base_power ? @modify(l -> floor(l - 0.01) - 0.01, log(t.base, $__)) : __
+            max(vmin, __)
+        end
+        ticks = filter!(∈((vmin..vmax) ∩ (scale.vmin..scale.vmax)), Real[
+            reverse(Makie.get_tickvalues(t, vmin, -mintick));
+            0;
+            Makie.get_tickvalues(t, mintick, vmax);
+        ])
+        if length(ticks) ≥ 2
+            ticks
+        else
+            if !go_to_previous_base_power
+                Makie.get_tickvalues(t, scale, vmin, vmax; go_to_previous_base_power=true)
+            else
+                Makie.get_tickvalues(WilkinsonTicks(5), vmin, vmax)
+            end
+        end
+    else
+        for subs in [[1], [1,3], [1,2,5], [1,2,3,5], 1:9]
+            ticks = Makie.get_tickvalues((@set t.subs = subs), scale, vmin, vmax; go_to_previous_base_power)
+            length(ticks) ≥ t.k_min && return ticks
+        end
+        return ticks
     end
-    ticks = filter!(∈((vmin..vmax) ∩ (scale.vmin..scale.vmax)), [
-        reverse(Makie.get_tickvalues(t, vmin, -mintick));
-        0.;
-        Makie.get_tickvalues(t, mintick, vmax);
-    ])
-    length(ticks) ≤ 1 && !go_to_previous_base_power ?
-        Makie.get_tickvalues(t, scale, vmin, vmax; go_to_previous_base_power=true) :
-        ticks
 end
 
 Makie.get_minor_tickvalues(t::BaseMulTicks, scale, tickvals, vmin, vmax) = Makie.get_tickvalues(t, scale, vmin, vmax)
