1ed5eb893eeebf7bd42b6a1a9761f89ae1cd41ce
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    add primary_resolution()

--- ext/GLMakieExt.jl
+++ ext/GLMakieExt.jl
@@ -1,6 +1,9 @@
 module GLMakieExt
 
 using MakieExtra: show_gl_icon_in_dock
+import MakieExtra: primary_resolution
+import GLMakie
+
 
 function __init__()
     if Sys.isapple()
@@ -10,6 +13,13 @@ function __init__()
             @warn "Failed to hide the GL icon from the dock" (e, catch_backtrace())
         end
     end
+end
+
+
+function primary_resolution()
+    monitor = GLMakie.GLFW.GetPrimaryMonitor()
+    videomode = GLMakie.MonitorProperties(monitor).videomode
+    return (videomode.width, videomode.height)
 end
 
 end
