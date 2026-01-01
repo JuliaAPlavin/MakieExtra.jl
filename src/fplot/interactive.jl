6a6cad7aa95fd2819a1e46110ffdef3177d2f54c
Alexander Plavin <alexander@plav.in>
2026-01-01 00:00:00 +0000

    fix axis passed to widgets

--- src/fplot/interactive.jl
+++ src/fplot/interactive.jl
@@ -1,12 +1,17 @@
 abstract type FPlotAddon end
 
-add!(addon::FPlotAddon, fplt::FPlot, plt::Plot; kwargs...) = add!(current_axis(), addon, fplt, plt; kwargs...)
-
 with_widgets(plotf, ws) = function (args...; kwargs...)
     plt = plotf(args...; kwargs...)
     fplt = filteronly(a -> a isa Union{FPlot,Observable{<:FPlot}}, args) |> to_value
+    ax = if hasproperty(plt, :axis)
+        plt.axis
+    elseif !isempty(args) && args[1] isa Axis
+        args[1]
+    else
+        current_axis()
+    end
     for w in ws
-        add!(w, fplt, plt; kwargs...)
+        add!(ax, w, fplt, plt; kwargs...)
     end
     return plt
 end
